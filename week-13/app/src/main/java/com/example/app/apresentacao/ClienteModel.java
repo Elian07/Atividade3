23570dc8c4bd343fc9dceabb472e2c29d04c236d
Elian <elians.2016@alunos.utfpr.edu.br>
2019-11-06 20:39:45 -0300

    Camada de serviços

---
+++ week-13/app/src/main/java/com/example/app/apresentacao/ClienteModel.java
@@ -0,0 +1,18 @@
+package com.example.app.apresentacao;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaisModel {
+    
+    private Long id;
+    private String nome;
+    private Double limiteCredito;
+    private Pais pais;
+}
